af757fcc5a2f50a3b6ec8781f4e9100856bd1cef
Mario Mario <marioramos083@gmail.com>
2021-02-10 09:15:01 -0500

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,2 +1,68 @@
-﻿CREATE DATABASE ticket;
+﻿--CREATE DATABASE ticket;
 
+ CREATE TABLE empresa(
+     rut text not null primary key,
+     razon_social text not null
+ );
+
+  CREATE TABLE tienda(
+     codigo text not null primary key,
+     rut text not null,
+     direccion text not null,
+     telefono text,
+     l_inicio_atencion time,
+     l_fin_atencion time,
+     m_inicio_atencion time,
+     m_fin_atencion time,
+     mm_inicio_atencion time,
+     mm_fin_atencion time,
+     j_inicio_atencion time,
+     j_fin_atencion time,
+     v_inicio_atencion time,
+     v_fin_atencion time,
+     s_inicio_atencion time,
+     s_fin_atencion time,
+     d_inicio_atencion time,
+     d_fin_atencion time,
+     CONSTRAINT tienda_fk FOREIGN KEY (rut)
+      REFERENCES empresa (rut) MATCH FULL
+      ON UPDATE CASCADE ON DELETE NO ACTION
+ );
+
+  CREATE TABLE totem(
+     codigo text not null primary key,
+     codigo_tienda text not null,
+     ubicacion text not null,
+     CONSTRAINT totem_fk FOREIGN KEY (codigo_tienda)
+      REFERENCES tienda (codigo) MATCH FULL
+      ON UPDATE CASCADE ON DELETE NO ACTION
+ );
+
+ CREATE TABLE tipo_operacion(
+     codigo text not null primary key,
+     codigo_totem text not null,
+     descripcion text not null,
+     CONSTRAINT tipo_operacion_fk FOREIGN KEY (codigo_totem)
+      REFERENCES totem (codigo) MATCH FULL
+      ON UPDATE CASCADE ON DELETE NO ACTION
+ );
+
+CREATE TABLE ticket(
+     secuencial serial not null primary key,
+     codigo_tipo_operacion text not null,
+     fecha_sacado timestamp not null,
+     CONSTRAINT ticket_fk FOREIGN KEY (codigo_tipo_operacion)
+      REFERENCES tipo_operacion (codigo) MATCH FULL
+      ON UPDATE CASCADE ON DELETE NO ACTION
+ );
+
+ CREATE TABLE usuario(
+     codigo text not null primary key,
+     rut text not null,
+     nombre text not null,
+     apellidos text not null,
+     telefono text not null,
+     CONSTRAINT usuario_fk FOREIGN KEY (rut)
+      REFERENCES empresa (rut) MATCH FULL
+      ON UPDATE CASCADE ON DELETE NO ACTION
+ );

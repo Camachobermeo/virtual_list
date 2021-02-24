be0f3d333cf5035eb564ccd7faef9cfc5056e61d
Mario Mario <marioramos083@gmail.com>
2021-02-24 09:48:56 -0500

    Update app-config.ts

--- src/app/servicios/app-config.ts
+++ src/app/servicios/app-config.ts
@@ -7,6 +7,8 @@ import { Injectable } from '@angular/core';
 export class AppConfig {
 
     public baseApiPath: string = "http://localhost/virtual-list-backend/";
+    // public baseApiPath: string = "https://warm-mesa-04446.herokuapp.com/";
+    // public baseApiPath: string = "https://virtual-list.000webhostapp.com/";
     
     constructor() {
 
